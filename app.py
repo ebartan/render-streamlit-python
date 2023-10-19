83edc40920612995c49f24f46c3e3245baedc424
Eray Bartan <ebartan@gmail.com>
2023-10-19 17:30:24 +0000

    No-code & Full Stack Developer

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
- 
+st.caption('Hi 👋, I'm ebartan - A LLM Resercher & No-code & Full Stack Developer From Bodrum/Turkey')
 st.write("""
 # Welcome Eray Bartan
 Hello *world!*
