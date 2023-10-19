bfca225ed64170811d6075eb0ca31bd5f4be230c
Eray Bartan <ebartan@gmail.com>
2023-10-19 18:00:34 +0000

    adımız görünsün

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 st.caption('Hi 👋, I am ebartan - A LLM Resercher & No-code & Full Stack Developer From Yalikavak/Bodrum/Turkey')
 st.write("""
-# LLM resercher like Langchain Cohere Google PaLM
+# Eray Bartan
 As an OpenAI GPT Implementer and Expert Full Stack Developer with 18 years of experience in the municipality sector, I specialize in no-code and low-code solutions to streamline workflows and enhance user experiences. *Thanks!*
 """)
