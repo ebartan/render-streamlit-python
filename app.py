e3256b9934b8253b8e189a54cf448f749ece6f7a
Eray Bartan <ebartan@gmail.com>
2023-10-19 19:12:13 +0000

    #

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ st.write("""
 # Eray Bartan
 As an OpenAI GPT Implementer and Expert Full Stack Developer with 18 years of experience in the municipality sector, I specialize in no-code and low-code solutions to streamline workflows and enhance user experiences. *Thanks!*
 """)
+st.write("""# topics I've been dealing with in recent days""")
